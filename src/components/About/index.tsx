2c8243759e7c16f90b180c5516fb80439597edbd
enosintech <enos.nsambaj@gmail.com>
2024-12-27 12:38:22 +0200

    fix lint error

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -45,7 +45,7 @@ const About = () => {
   return (
     <section style={{ height: height ? height * 2 : "200svh" }} className='w-full about bg-black relative z-20 px-2 md:px-4'>
         <div className={cn('w-full h-1/2 pb-12 max-[500px]:text-[7vw] text-[6vw] sm:text-[5vw] md:text-[4vw] xl:text-[3vw] leading-none flex flex-col justify-evenly', NohemiLight.className)}>
-            <p className='max-w-[96%] max-[490px]:max-w-[100%] sm:max-w-[92%] lg:max-w-[80%] text-zinc-800 aboutText'>I'm Enos, a creative software engineer and lifelong learner specialized in developing highly performant and interactive websites using TypeScript.</p>
+            <p className='max-w-[96%] max-[490px]:max-w-[100%] sm:max-w-[92%] lg:max-w-[80%] text-zinc-800 aboutText'>I&apos;m Enos, a creative software engineer and lifelong learner specialized in developing highly performant and interactive websites using TypeScript.</p>
             <p className='max-w-[95%] max-[490px]:max-w-[95%] sm:max-w-[85%] lg:max-w-[75%] text-zinc-900 aboutText'>I have an orientation for detail and symmetric design. I love when things fit and I have a keen eye for when they dont.</p>
             <p className='max-w-[90%] max-[490px]:max-w-[100%] sm:max-w-[80%] md:max-w-[65%] lg:max-w-[55%] text-zinc-950 aboutText'>Currently working as a Freelancer based in Lusaka, Zambia.</p>
         </div>
