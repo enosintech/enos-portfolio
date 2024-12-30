43474210dd6c79fd878d264e3959be0a15736c74
enosintech <enos.nsambaj@gmail.com>
2024-12-30 18:46:45 +0200

    final push'
    '

--- src/components/WorkListing/index.tsx
+++ src/components/WorkListing/index.tsx
@@ -29,19 +29,19 @@ const WorkListing = () => {
           </div>
 
           <div className='w-full h-[90%] gap-1 flex lg:flex-row flex-col'>
-            <div className="lg:w-[60%] w-full lg:h-full h-[50%] flex items-center justify-center">
+            <div className="lg:w-[80%] w-full lg:h-full h-[65%] flex items-center justify-center">
               <div className="max-w-full w-fit h-fit max-h-full rounded-[5px] md:rounded-[6px] overflow-hidden deep-shadow">
                 <video id="video" className='w-full h-full object-contain' autoPlay={true} loop={true} controls={false} playsInline={true} muted={true}>
                   <source src={"/videos/makeupbyram.mp4"} type="video/mp4"/>
                 </video>
               </div>
             </div>
-            <div className="lg:w-[40%] w-full lg:h-full h-[50%] flex flex-col justify-between px-1 py-2 lg:py-12 items-end">
-              <p className={cn("xl:max-w-[80%] text-justify lg:text-right text-sm sm:text-base md:text-lg lg:text-sm lg:max-w-[60%]", NohemiLight.className)}>Makeupbyram is a Kuala Lumpur-based makeup artist specializing in bold, creative shoot makeup, high-fashion runway looks, and edgy, statement-making styles.</p>
+            <div className="lg:w-[20%] w-full lg:h-full h-[35%] flex flex-col justify-between px-1 py-2 items-end">
+              <div></div>
               <div className="text-right">
                 <p className="text-neutral-500 text-xs">Tech</p>
 
-                <div className={cn("md:text-sm text-xs mt-1", NohemiSemiBold.className)}>
+                <div className={cn("text-sm mt-1", NohemiSemiBold.className)}>
                   <p>Next.JS</p>
                   <p>Tailwind CSS</p>
                   <p>GSAP</p>
