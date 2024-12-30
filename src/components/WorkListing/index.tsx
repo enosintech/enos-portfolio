8cb40b0363d27e0781a245a2aa23de4fa47cc7b0
enosintech <enos.nsambaj@gmail.com>
2024-12-30 18:31:20 +0200

    on track

--- src/components/WorkListing/index.tsx
+++ src/components/WorkListing/index.tsx
@@ -1,4 +1,4 @@
-import { NohemiBold } from "@/lib/fonts";
+import { NohemiBold, NohemiLight, NohemiSemiBold } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { ArrowUpRight } from "lucide-react";
 
@@ -28,16 +28,25 @@ const WorkListing = () => {
 
           </div>
 
-          <div className='w-full h-[90%] gap-1 flex md:flex-row flex-col'>
-            <div className="md:w-[60%] w-full md:h-full h-[50%] bg-slate-200 flex items-center justify-center">
-              <div className="max-w-[90%] w-fit h-fit max-h-[85%] rounded-[5px] md:rounded-[6px] overflow-hidden deep-shadow">
+          <div className='w-full h-[90%] gap-1 flex lg:flex-row flex-col'>
+            <div className="lg:w-[60%] w-full lg:h-full h-[50%] flex items-center justify-center">
+              <div className="max-w-full w-fit h-fit max-h-full rounded-[5px] md:rounded-[6px] overflow-hidden deep-shadow">
                 <video id="video" className='w-full h-full object-contain' autoPlay={true} loop={true} controls={false} playsInline={true} muted={true}>
                   <source src={"/videos/makeupbyram.mp4"} type="video/mp4"/>
                 </video>
               </div>
             </div>
-            <div className="md:w-[40%] w-full md:h-full h-[50%] bg-slate-200">
-             
+            <div className="lg:w-[40%] w-full lg:h-full h-[50%] flex flex-col justify-between px-1 py-2 lg:py-12 items-end">
+              <p className={cn("xl:max-w-[80%] text-justify lg:text-right text-sm sm:text-base md:text-lg lg:text-sm lg:max-w-[60%]", NohemiLight.className)}>Makeupbyram is a Kuala Lumpur-based makeup artist specializing in bold, creative shoot makeup, high-fashion runway looks, and edgy, statement-making styles.</p>
+              <div className="text-right">
+                <p className="text-neutral-500 text-xs">Tech</p>
+
+                <div className={cn("md:text-sm text-xs mt-1", NohemiSemiBold.className)}>
+                  <p>Next.JS</p>
+                  <p>Tailwind CSS</p>
+                  <p>GSAP</p>
+                </div>
+              </div>
             </div>
           </div>
 
