80564299ebab3ceb3d32d8e10d673f9a1fd6e29a
enosintech <enos.nsambaj@gmail.com>
2024-12-30 11:51:30 +0200

    fix work vid component

--- src/components/WorkListing/index.tsx
+++ src/components/WorkListing/index.tsx
@@ -29,10 +29,12 @@ const WorkListing = () => {
           </div>
 
           <div className='w-full h-[90%] gap-1 flex md:flex-row flex-col'>
-            <div className="md:w-[60%] w-full md:h-full h-[50%] bg-slate-200 grid place-items-center">
-              <video id="video" className='max-w-[90%] w-fit h-fit max-h-[85%] object-contain rounded-[5px] md:rounded-[6px] deep-shadow' autoPlay={true} loop={true} controls={false} playsInline={true} muted={true}>
-                <source src={"/videos/makeupbyram.mp4"} type="video/mp4"/>
-              </video>
+            <div className="md:w-[60%] w-full md:h-full h-[50%] bg-slate-200 flex items-center justify-center">
+              <div className="max-w-[90%] w-fit h-fit max-h-[85%] rounded-[5px] md:rounded-[6px] overflow-hidden deep-shadow">
+                <video id="video" className='w-full h-full object-contain' autoPlay={true} loop={true} controls={false} playsInline={true} muted={true}>
+                  <source src={"/videos/makeupbyram.mp4"} type="video/mp4"/>
+                </video>
+              </div>
             </div>
             <div className="md:w-[40%] w-full md:h-full h-[50%] bg-slate-200">
              
